0665bc076132d1f3efe702cf3a939ebee10117e1
Sebastian <119546442+Sebhvarg@users.noreply.github.com>
2024-08-08 21:54:02 -0500

    Initial commit

---
+++ src/main/java/com/espol/dds/grupo6/rpmgame/RPMGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.espol.dds.grupo6.rpmgame;
+
+/**
+ *
+ * @author sebas
+ */
+public class RPMGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
